cea92b7252ea7f1970c7f9a3ed138f5fa1fec684
VirgushAA <126845952+VirgushAA@users.noreply.github.com>
2025-09-22 20:52:46 +0700

    started spa, buttons not ideal

--- web/static/scripts/main.js
+++ web/static/scripts/main.js
@@ -1,12 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("note-form");
+    const notesForm = document.getElementById("note-form");
+    const signUpForm = document.getElementById("sign-up-form");
+    const loginForm = document.getElementById("login-form");
     const notesList = document.getElementById("notes-list");
 
-    form.addEventListener("submit", (e) => {
+    notesForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        const textarea = form.querySelector("textarea");
+        const textarea = notesForm.querySelector("textarea");
         const noteText = textarea.value.trim();
 
         if (noteText) {
@@ -17,6 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
             textarea.value = "";
             textarea.style.height = "auto";
         }
+    });
+
+    signUpForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        pass;
+    })
+
+    document.getElementById('switch-to-login').addEventListener('click', () => {
+        document.getElementById('sign-up-area').style.display = 'none';
+        document.getElementById('login-area').style.display = 'block';
+    })
+
+    loginForm.addEventListener("submit", (e)=> {
+        e.preventDefault();
+        pass;
+    })
+
+    document.getElementById("switch-to-signup").addEventListener("click", () => {
+        document.getElementById("login-area").style.display = "none";
+        document.getElementById("sign-up-area").style.display = "block";
     });
 
     document.querySelectorAll("textarea").forEach((ta) => {
