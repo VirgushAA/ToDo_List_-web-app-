f5e3d45c7997210b66edd12299e68dde73361b19
VirgushAA <126845952+VirgushAA@users.noreply.github.com>
2025-09-24 01:32:39 +0700

    schemas db start

--- backend/routes/notes.py
+++ backend/routes/notes.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from backend.main import get_db
-from backend.models import note
+from backend.models import schemas
 
 router = APIRouter(prefix='/notes', tags=['notes'])
 
 @router.post('/add')
-def add_note(note_: note.NoteIn, conn=Depends(get_db)):
+def add_note(note_: schemas.NoteIn, conn=Depends(get_db)):
     # adding note to db, return response by default i gues
     cur = conn.cursor()
     cur.execute(''' INSERT INTO notes (note)
